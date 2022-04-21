c4027c8d9508f766ad127fbda44a60a738ab5371
roc <roclau0102@gmail.com>
2022-04-22 01:27:31 +0800

    hw1

---
+++ Assignment1/代码框架/main.cpp
@@ -0,0 +1,190 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <eigen3/Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include <cmath>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the model matrix for rotating the triangle around the Z axis.
+    // Then return it.
+
+    float theta = rotation_angle / 180.0f * acos(-1); // angle to radius
+    Eigen::Matrix4f rotation;
+    rotation << std::cos(theta), -std::sin(theta), 0, 0,
+         std::sin(theta),  std::cos(theta), 0, 0,
+         0, 0, 1, 0,
+         0, 0, 0, 1;
+    model = rotation * model;
+    return model;
+}
+
+Eigen::Matrix4f get_rotation(Eigen::Vector3f axis, float angle)
+{
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+
+    // todo: 绕任意过原点的轴旋转
+
+    return rotation;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    // TODO: Implement this function
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+
+    // 变换透视空间 to 正交空间
+    Eigen::Matrix4f perspective;
+    perspective << zNear, 0, 0, 0,
+                    0, zNear, 0, 0,
+                    0, 0, zNear + zFar, -zNear * zFar,
+                    0, 0, 1, 0;
+    
+    float theta = eye_fov / 2.0f / 180.0f * std::acos(-1);
+    float top = std::tan(theta) * zNear;
+    float bottom = -top;
+    float right = aspect_ratio * top;
+    float left = -right;
+
+    // 移动正交空间 to 原点 
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -(left + right) / 2.0f,
+                    0, 1, 0, -(top + bottom) / 2.0f,
+                    0, 0, 1, -(zNear + zFar) / 2.0f,
+                    0, 0, 0, 1;
+
+    // 变换正交空间 to 'canonical' cube
+    Eigen::Matrix4f scale;
+    scale << 2.0f / (right - left), 0, 0, 0,
+                0, 2.0f / (top - bottom), 0, 0,
+                0, 0, 2.0f / (zFar - zNear), 0,
+                0, 0, 0, 1;
+
+    // 完整的透视变换
+    projection = scale * translate * perspective;
+    return projection;
+}
+
+// Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+//                                       float zNear, float zFar)
+// {
+//     // Students will implement this function
+
+//     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+//     // TODO: Implement this function
+//     // Create the projection matrix for the given parameters.
+//     // Then return it.
+    
+//     eye_fov = eye_fov * MY_PI / 180;
+//     float fax = 1.0f / (float)tan(eye_fov * 0.5f);
+
+//     Eigen::Matrix4f perspective;
+//     perspective << (float)(fax / aspect_ratio),0,0,0,
+//                     0,(float)(fax),0,0,
+//                     0,0,zFar / (zFar - zNear),0,
+//                     0,0,1,-zNear * zFar / (zFar - zNear);
+
+//     projection = perspective * projection;
+
+//     return projection;
+// }
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3) {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4) {
+            filename = std::string(argv[3]);
+        }
+        else if (argc > 4)
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27) {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a') {
+            angle += 10;
+        }
+        else if (key == 'd') {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
